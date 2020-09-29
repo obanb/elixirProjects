c04c516e0c387cddb8384e54fc4a876d6ec11fa7
obanb <ondrej.basista@apitree.cz>
2020-09-29 23:15:52 +0200

    player move

--- core/lib/core/game.ex
+++ core/lib/core/game.ex
@@ -5,11 +5,23 @@ defmodule Core.Game do
     number_of_players: 0,
     state: :initializing,
     board_size_x: 10,
-    board_size_y: 10
+    board_size_y: 10,
+    player1: %{
+      position_x: 0,
+      position_y: 0
+    },
+    player2: %{
+      position_x: 0,
+      position_y: 0
+    }
   ]
 
   def get_state do
     %Game{}.state
+  end
+
+  def move(player, to) do
+
   end
 
 end
